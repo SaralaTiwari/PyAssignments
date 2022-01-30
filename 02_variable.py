4a78ba3f28da47f258bed0056ec9b6cae71f5a3b
Sarala  Tiwari <saralatiwari@Saralas-MacBook-Air.local>
2022-01-29 22:29:27 -0500

    Completed Assignment 1

---
+++ 02_variable.py
@@ -0,0 +1,33 @@
+"""
+© https://sudipghimire.com.np
+
+
+Variables exercises
+
+
+Please read the note carefully and try to solve the problem below:
+
+1. Assign 55 to the variable `x` and print it in the console
+2. change the value of variable to be 60
+3. add x by 10 again
+
+note: please take the reference from the lab of datatypes "/src/02_data_types/01_variables.py"
+note: please try to print the value of the variable in each and every steps or questions
+
+"""
+
+#  answer 1
+x = 55
+print(x)
+
+
+#  answer 2
+x = 60 
+print(x)
+#  answer 3
+x = x + 10 
+print(x)
+
+#  answer 4
+
+#  answer 5
